fa8644c4b375f9cc7ac41fcf1972e4d146034281
SunnyKumar <ssrivastava@pronoesis.com>
2016-05-21 14:20:00 +0530

    changed total hits

--- index.php
+++ index.php
@@ -213,7 +213,7 @@
 
       <!--  FOOTER  -->
       <footer>
-		<p id="userHit" class="pull-left" style="font-family: monospace;font-weight: bold;">Total Hit till now : 22</p>
+		<p id="userHit" class="pull-left" style="font-family: monospace;font-weight: bold;">Total Hit till now : 189</p>
         <p class="pull-right"><a href="#">Back to top</a></p>
         <!--  <p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>  -->
       </footer>
